e29eb2e6cfca83dab496da49113ecbcef8a00a0c
Romil003 <romilghadge33@gmail.com>
2023-01-04 18:56:47 +0530

    Added : display simple message

---
+++ src/main/java/com/bridgelabz/helloworldspringapp/Controller/HelloRestController.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.helloworldspringapp.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloRestController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Hello from BridgeLabz";
+    }
+}
